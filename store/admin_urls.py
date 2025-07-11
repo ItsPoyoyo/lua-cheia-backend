f793dff9fec1bab9c1f015464b58b54816288a39
poyoyo 🐆 <gazouinihussein@gmail.com>
2025-07-11 11:45:22 -0300

    0

--- store/admin_urls.py
+++ store/admin_urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-
 from . import admin_views
 
 urlpatterns = [
-    path('', admin_views.sales_dashboard, name='sales_dashboard'),
-    path('analytics/', admin_views.sales_analytics, name='sales_analytics'),
-
+    path('', admin_views.sales_dashboard, name='admin:sales_dashboard'),
+    path('analytics/', admin_views.sales_analytics, name='admin:sales_analytics'),
 ]
 
